792c5b714ea027f93d41b97feb68602b4865f3fa
Hamilton Vivas <alextrinvivas@gmail.com>
2024-09-24 21:31:19 -0300

    adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,38 @@ try {
       res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+    // console.log(req.params.id);
+    const id = Number(req.params.id);
+
+    try {
+    const movie = await prisma.movie.findUnique({
+        where: {
+            id:id
+        }
+    });
+
+    if(!movie) {
+        return res.status(404).send({message: "Filme não encotrado"});
+    }
+
+    const data = { ...req.body };
+    //console.log(data);
+    data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+    
+
+    await prisma.movie.update({
+        where: { id: id},
+        data: data
+      });
+    } catch(error) {
+       return res.status(500).send({message: "Falha ao atualizar o registro do filme"}) 
+    }
+
+    res.status(200).send();
+    
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
 });
